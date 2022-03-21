1964f4eafdba8d53228b5408051c3fdb05fb93fc
Esteban Canales <ecanales1@csustan.edu>
2022-03-20 23:39:41 -0700

    First commit

--- functions.js
+++ functions.js
@@ -1,3 +1,25 @@
-function input(){
-    const   input1 = document.getElementById()
+
+//this is what is going to add the two numbers
+
+// parse int is suppose to help keeping the numbers a actual number rather than a string
+
+
+
+//sticky menu
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
 }
+
+
+//console log message
+
+console.log("hello");
